efbec76c7892144994ed5140ebfcf89b121ee3bc
Dmytro Minyaylo <dimon9660@gmail.com>
2025-03-28 16:33:19 +0200

    added related_name attribute

--- db/models.py
+++ db/models.py
@@ -66,12 +66,20 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"{self.created_at.strftime(r"%Y-%m-%d %H:%M:%S")}"
+        return f"{self.created_at}"
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey("MovieSession", on_delete=models.CASCADE)
-    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    movie_session = models.ForeignKey(
+        "MovieSession",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
     row = models.IntegerField(default=0, blank=True, null=True)
     seat = models.IntegerField(default=0, blank=True, null=True)
 
